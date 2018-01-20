0b191b8eaf6842f4a558027611917990930c2c27
Anwesh43 <anweshthecool0@gmail.com>
2018-01-21 02:37:05 +0530

    adding NumberNodeList

--- numberspinnerview/src/main/java/ui/anwesome/com/numberspinnerview/NumberSpinnerView.kt
+++ numberspinnerview/src/main/java/ui/anwesome/com/numberspinnerview/NumberSpinnerView.kt
@@ -22,12 +22,45 @@ class NumberSpinnerView(ctx:Context,var n:Int = 12):View(ctx) {
     data class NumberNode(var number:Int) {
         var next:NumberNode?=null
         var prev:NumberNode?=null
-        fun drawPosText(canvas:Canvas,paint:Paint,scale:Float,x:Float,y:Float,w:Float) {
+        fun drawText(canvas:Canvas,paint:Paint,scale:Float,x:Float,y:Float,w:Float,dir:Int) {
             paint.color = Color.WHITE
-            canvas.drawRoundRect(RectF(x,y-w*scale,x+w,y+w-w*scale),w/10,w/10,paint)
+            canvas.drawRoundRect(RectF(x,y-dir*w*scale,x+w,y+w-dir*w*scale),w/10,w/10,paint)
             paint.textSize = w/2
             paint.color = Color.BLACK
             canvas.drawText("$number",x+w/2,y+paint.textSize/4,paint)
+        }
+    }
+    data class NumberNodeList(var w:Float,var h:Float,var n:Int,var mode:Int = 0) {
+        var root:NumberNode = NumberNode(0)
+        var currNode:NumberNode? = root
+        var prevNode:NumberNode?=null
+        init {
+            var node = root
+            for(i in 1..n) {
+                val curr = NumberNode(i)
+                node.next = curr
+                curr.prev = node
+                node = curr
+            }
+            node.next = root
+            root.prev = node
+        }
+        fun draw(canvas:Canvas,paint:Paint) {
+            currNode?.drawText(canvas,paint,1f,w/2-w/10,h/2-w/10+(w/5)*mode,w/5,mode)
+            prevNode?.drawText(canvas,paint,1f,w/2-w/10,h/2-w/10,w/5,mode)
+        }
+        fun startUpdating(dir:Int,startcb:()->Unit) {
+            if(mode == 0) {
+                mode = dir
+                prevNode = currNode
+                when(dir) {
+                    1 -> currNode = currNode?.next
+                    -1 -> currNode = currNode?.prev
+                }
+            }
+        }
+        fun update(stopcb:()->Unit) {
+
         }
     }
 }
