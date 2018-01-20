c28ef8f112e5cb12d741c722cebd891fc4523420
Anwesh43 <anweshthecool0@gmail.com>
2018-01-21 03:22:24 +0530

    using NumberSpinnerView in Activity

--- app/src/main/java/ui/anwesome/com/kotlinnumberspinnerview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinnumberspinnerview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinnumberspinnerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.numberspinnerview.NumberSpinnerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        NumberSpinnerView.create(this)
     }
 }
