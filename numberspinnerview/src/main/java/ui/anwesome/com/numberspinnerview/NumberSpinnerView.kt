7577a04c5e7165f336383b2af7da8d0b64654af2
Anwesh43 <anweshthecool0@gmail.com>
2018-01-21 02:02:34 +0530

    adding numbernode

--- numberspinnerview/src/main/java/ui/anwesome/com/numberspinnerview/NumberSpinnerView.kt
+++ numberspinnerview/src/main/java/ui/anwesome/com/numberspinnerview/NumberSpinnerView.kt
@@ -6,7 +6,7 @@ package ui.anwesome.com.numberspinnerview
 import android.view.*
 import android.graphics.*
 import android.content.*
-class NumberSpinnerView(ctx:Context):View(ctx) {
+class NumberSpinnerView(ctx:Context,var n:Int = 12):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
 
@@ -18,5 +18,16 @@ class NumberSpinnerView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class NumberNode(var number:Int) {
+        var next:NumberNode?=null
+        var prev:NumberNode?=null
+        fun drawPosText(canvas:Canvas,paint:Paint,scale:Float,x:Float,y:Float,w:Float) {
+            paint.color = Color.WHITE
+            canvas.drawRoundRect(RectF(x,y-w*scale,x+w,y+w-w*scale),w/10,w/10,paint)
+            paint.textSize = w/2
+            paint.color = Color.BLACK
+            canvas.drawText("$number",x+w/2,y+paint.textSize/4,paint)
+        }
     }
 }
