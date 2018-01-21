225badc36c52e4e46326621868feb7fbb5d38bf4
Anwesh43 <anweshthecool0@gmail.com>
2018-01-21 11:51:48 +0530

    adding onNumberSelectedListener in activity

--- app/src/main/java/ui/anwesome/com/kotlinnumberspinnerview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinnumberspinnerview/MainActivity.kt
@@ -2,12 +2,16 @@ package ui.anwesome.com.kotlinnumberspinnerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.numberspinnerview.NumberSpinnerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        NumberSpinnerView.create(this)
+        var view = NumberSpinnerView.create(this)
+        view.addOnNumberSelectedListener {
+            Toast.makeText(this,"number selected $it",Toast.LENGTH_SHORT).show()
+        }
     }
 }
